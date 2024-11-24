21812796da5a427e50867806e16ad419fd94b1f8
Snak <72hoshino.snak@gmail.com>
2024-11-24 07:41:04 +0000

    エンドポイントを修正する

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 const app: express.Express = express();
 const port = 3003;
 
-app.get("/v1/dice/", (_req, res) => {
+app.get("/api/v1/dice/", (_req, res) => {
   res.status(200).send({ value: Math.floor(Math.random() * 6) + 1 });
 });
 
