3aaec5dbcfcf2f0bd99ab27774cd115e0e2038c6
Snak <72hoshino.snak@gmail.com>
2024-11-24 05:48:02 +0000

    ポート番号を変更する

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const app: express.Express = express();
-const port = 3002;
+const port = 3003;
 
 app.get("/", (_req, res) => {
   res.status(200).send({ i: 3, j: 19 });
