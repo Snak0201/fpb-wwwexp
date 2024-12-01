9df6d1c619bb5a9d92c62fe73b99f78884f839c6
Snak <snak.hoshinonaka@gmail.com>
2024-12-01 01:37:39 +0000

    n個の数値を返せるように修正する

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,27 @@ const app: express.Express = express();
 const port = 3003;
 
 // 1~6のサイコロをN個振る
-app.get("api/v1/dice/cube", (req: any, res: any) => {
+app.get("/api/v1/dice/cube/", (req: any, res: any) => {
   const n = parseInt(String(req.query.n), 10);
 
   if (isNaN(n) || n < 1) {
     return res
       .status(400)
-      .send("クエリパラメータnに1以上の数値を入れてください。");
+      .send({ message: "クエリパラメータnに100以下の数値を入れてください。" });
   }
 
-  return res.status(200).send({ value: Math.floor(Math.random() * 6) + 1 });
+  if (n > 100) {
+    return res
+      .status(400)
+      .send({ message: "クエリパラメータnに100以下の数値を入れてください。" });
+  }
+
+  const values = Array.from(
+    { length: n },
+    () => Math.floor(Math.random() * 6) + 1
+  );
+
+  return res.status(200).send({ values: values });
 });
 
 app.listen(port, () => {
