33167e32dd4759e95ec7dd53069a0750b1d8f965
Snak <snak.hoshinonaka@gmail.com>
2024-12-01 01:24:23 +0000

    立方体サイコロのAPIを追加する

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@ import express from "express";
 const app: express.Express = express();
 const port = 3003;
 
-app.get("/api/v1/dice/", (_req, res) => {
-  res.status(200).send({ value: Math.floor(Math.random() * 6) + 1 });
+// 1~6のサイコロをN個振る
+app.get("api/v1/dice/cube", (req: any, res: any) => {
+  const n = parseInt(String(req.query.n), 10);
+
+  if (isNaN(n) || n < 1) {
+    return res
+      .status(400)
+      .send("クエリパラメータnに1以上の数値を入れてください。");
+  }
+
+  return res.status(200).send({ value: Math.floor(Math.random() * 6) + 1 });
 });
 
 app.listen(port, () => {
